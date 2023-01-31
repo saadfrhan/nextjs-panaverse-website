e551867ad24393244741d7056e412a6d5677d26f
saadfrhan <saadfarhan347@gmail.com>
2023-01-31 05:48:20 +0500

    feat: remove packages

--- src/utils/post_gen.ts
+++ src/utils/post_gen.ts
@@ -1,8 +1,5 @@
 import path from 'path';
-import { remark } from 'remark';
-import html from 'remark-html';
 import fs from 'fs'
-import matter from 'gray-matter'
 
 export async function getPostData(id: string) {
   const fullPath = path.join("C:\\Users\\hp\\Downloads\\development\\nextjs-projects\\nextjs-panaverse-website\\src\\constants\\course_outline", `${id}.md`);
