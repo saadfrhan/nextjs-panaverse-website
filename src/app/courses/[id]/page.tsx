abfc5586defd76acd0b31a3d5319c1cf3371d5b4
saadfrhan <saadfarhan347@gmail.com>
2023-01-31 23:15:08 +0500

    feat: update

--- src/app/courses/[id]/page.tsx
+++ src/app/courses/[id]/page.tsx
@@ -3,6 +3,11 @@ import React from "react";
 import Markdown from "markdown-to-jsx";
 import styles from './course.module.css';
 import { getPostData } from "../../../utils/post_gen";
+import { course_slugs } from "@/constants/course";
+
+export async function generateStaticParams() {
+  return course_slugs.map((id) => ({ id }));
+}
 
 export default async function Course({ params }: {
   params: { id: string }
