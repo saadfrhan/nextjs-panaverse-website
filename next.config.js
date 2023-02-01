976093717594ef04ff7995410de9cccfbee07aa4
saadfrhan <saadfarhan347@gmail.com>
2023-02-01 06:12:26 +0500

    feat: revert from pwa

--- next.config.js
+++ next.config.js
@@ -5,18 +5,4 @@ const nextConfig = {
   }
 }
 
-/** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-})
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development'
-  }
-})
-
 module.exports = nextConfig
