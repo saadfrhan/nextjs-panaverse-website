4dfc3fdd3e0365eb884ed71a8c05119aee5c69c4
saadfrhan <saadfarhan347@gmail.com>
2023-02-15 16:43:53 +0500

    Update page.tsx

--- src/app/courses/specializations/[id]/page.tsx
+++ src/app/courses/specializations/[id]/page.tsx
@@ -1,8 +1,8 @@
 import CourseContainer from '@/components/courses/CourseContainer';
-import { course_slugs, specs } from '@/constants/course'
+import { specs } from '@/constants/course'
 
 export async function generateStaticParams() {
-  return course_slugs.map((id) => ({ id }));
+  return ["0", "1", "2", "3", "4", "5"].map((id) => ({ id }));
 }
 
 export default function Specialization({ params: { id } }: { params: { id: number } }) {
