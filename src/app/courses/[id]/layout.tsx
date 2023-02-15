7b79b224ae737a7af9807adfc82f880cba80b468
saadfrhan <saadfarhan347@gmail.com>
2023-02-16 03:24:23 +0500

    feat: update

--- src/app/courses/[id]/layout.tsx
+++ src/app/courses/[id]/layout.tsx
@@ -4,6 +4,7 @@ import { core_courses, specs } from "@/constants/course"
 import {
   Flex, Text, Box, useMediaQuery, Button, Icon, IconButton
 } from '@chakra-ui/react'
+import Link from "next/link"
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { TfiClose as CloseIcon } from 'react-icons/tfi'
@@ -16,19 +17,19 @@ export default function Layout({
 
   const { push } = useRouter();
 
-  const [isLandscape] = useMediaQuery('(min-width: 1000px)');
+  const [isLandscape] = useMediaQuery('(min-width: 1034px)');
 
   const [show, isShow] = useState(false);
 
   return (
-    <Flex direction={['column', 'column', 'column', 'row']} gap={7}>
-      {(isLandscape || show) && <Flex direction="column">
+    <Flex direction={['column', 'column', 'column', 'column', 'row']} gap={7} justifyContent="space-between">
+      {(isLandscape || show) && <Flex direction="column" w={["100%", "100%", "100%", "100%", "30%"]}>
         {!isLandscape && (
           <IconButton
             w={55} h={55}
             rounded={9}
-            pb={10}
-            right={5}
+            pt={10}
+            right={10}
             bg="transparent"
             position="absolute"
             onClick={() => isShow(!show)}
@@ -36,33 +37,41 @@ export default function Layout({
             icon={<Icon as={CloseIcon} />}
           />
         )}
-        <Box as="span" textAlign='left'>
-          Core Courses
-        </Box>
-        {core_courses.courses.map((c, i) =>
-          <Text pb={1} listStyleType="bullet" key={i} color='lightseagreen'
-            _hover={{ textDecoration: 'underline' }} cursor='pointer' onClick={() => push(c.slug)}>
-            Quarter {c.number} {c.heading}
-          </Text>)}
-        <Box as="span" textAlign='left'>
-          Specializations
-        </Box>
-
-        {specs.specializations.map((s, i) => (
-          <Flex key={i} pl={3} direction="column" paddingBlock={1.5}>
-            <Box as="span" textAlign='left'>
-              {s.heading}
-            </Box>
-            {s.courses.map((c, i) => (
-              <Text listStyleType="bullet" key={i} cursor="pointer" color='lightseagreen'
-                _hover={{ textDecoration: 'underline' }} mb={1}
-                onClick={() => push(`/courses/${c.slug}`)}
-              >
-                Quarter {c.number}  {c.heading}
+        <Flex direction="column" pb="5">
+          <Box as="span" textAlign='left' fontWeight="extrabold">
+            Core Courses
+          </Box>
+          {core_courses.courses.map((c, i) =>
+            <Link href={c.slug} key={i}>
+              <Text color='lightseagreen'
+                _hover={{ textDecoration: 'underline' }} cursor='pointer'>
+                Quarter {c.number} {c.heading}
               </Text>
-            ))}
-          </Flex>
-        ))}
+            </Link>)}
+        </Flex>
+
+        <Flex direction="column" pb="5">
+          <Box as="span" textAlign='left' fontWeight="bold">
+            Specializations
+          </Box>
+
+          {specs.specializations.map((s, i) => (
+            <Flex key={i} pl={3} direction="column" pb="5">
+              <Box as="span" textAlign='left'>
+                {s.heading}
+              </Box>
+              {s.courses.map((c, i) => (
+                <Link key={i} href={`/courses/${c.slug}`}>
+                  <Text cursor="pointer" color='lightseagreen'
+                    _hover={{ textDecoration: 'underline' }} mb={1}
+                  >
+                    Quarter {c.number}  {c.heading}
+                  </Text>
+                </Link>
+              ))}
+            </Flex>
+          ))}
+        </Flex>
 
       </Flex>}
       {!isLandscape && <Button
